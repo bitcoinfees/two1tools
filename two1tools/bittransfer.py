7668959342ee985cc10979cbfb4116732bd9bf2e
bitcoinfees <bitcoinfees@gmail.com>
2015-11-23 14:52:14 +0800

    Add methods to manually create and redeem bittransfers

--- two1tools/bittransfer.py
+++ two1tools/bittransfer.py
@@ -1,9 +1,12 @@
 import json
+import time
+import requests
+
 from two1.lib.bitserv.payment_methods import BitTransfer
 
 
 def get_bittransfer(request):
-    """Get the bittransfer dict.
+    """Get the bittransfer header from a request.
 
     Takes in the Flask request context object. Makes no assumptions about
     the validity of the payment.
@@ -20,3 +23,39 @@ def get_bittransfer(request):
         return json.loads(request.headers[BitTransfer.http_payment_data])
     except KeyError:
         return None
+
+
+def create_bittransfer(wallet, payer_username, payee_username, payee_address,
+                       amount, description=""):
+    """Manually create and sign a bittransfer.
+
+    wallet is a Wallet instance, payer_username is Config().username.
+    Refer to BitTransferRequests.make_402_payment.
+    """
+
+    bittransfer = json.dumps({
+        'payer': payer_username,
+        'payee_address': payee_address,
+        'payee_username': payee_username,
+        'amount': amount,
+        'timestamp': time.time(),
+        'description': description
+    })
+    signature = wallet.sign_message(bittransfer)
+    return bittransfer, signature
+
+
+def redeem_bittransfer(bittransfer, signature):
+    """Apply the result of create_bittransfer to effect the transfer.
+
+    Refer to BitTransfer.redeem_payment.
+    """
+    verification_url = BitTransfer.verification_url.format(
+        bittransfer['payee_username'])
+    try:
+        return requests.post(verification_url,
+                             data=json.dumps({'bittransfer': bittransfer,
+                                              'signature': signature}),
+                             headers={'content-type': 'application/json'})
+    except requests.ConnectionError:
+        pass
